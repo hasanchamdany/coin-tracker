c48acc47e3e03b5629707966de1a337dfd244f38
Muhammad Arif Hidayat <mhmdarif.developer@gmail.com>
2022-09-20 21:17:20 +0700

    feat(Category) : add entity Category

---
+++ GoCoin_WinForm/GoCoin_WinForm/Entity/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoCoin_WinForm.Entity
+{
+    class Category
+    {
+        // properties
+        public int Id { get; set; }
+        public string Category_name { get; set; }
+
+        //constructor
+        public Category() { }
+        public Category(int id, string category_name)
+        {
+            Id = id;
+            Category_name = category_name;
+        }
+    }
+}
